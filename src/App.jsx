e9f09ae78e27edf221b734ef767a2fd61f8f01c2
Sebastian Riquelme <sebarsalbo@gmail.com>
2023-01-13 01:07:22 -0300

    Projects mejorado

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { AboutMe } from "./components/AboutMe";
 import Hero from "./components/Hero";
 import { Projects } from "./components/Projects";
 import { Technologies } from "./components/Technologies";
-import { Projects } from "./components/Projects";
 
 function App() {
 	return (
@@ -12,7 +11,6 @@ function App() {
 			<Hero />
 			<AboutMe />
 			<Technologies />
-			<Projects2 />
 			<Projects />
 		</div>
 	);
