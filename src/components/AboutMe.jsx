d8d8bd38574e569cbf3907c657408b4b417e65ea
Sebastian Riquelme <sebarsalbo@gmail.com>
2023-01-15 04:35:08 -0300

    Diseño móvil corregido completo, falta animaciones, dark, nav y footer

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,20 +5,12 @@ export const AboutMe = () => {
 	return (
 		<div>
 			{/* About me, with image in left and text in right */}
-			<div className="flex flex-col-reverse md:flex-row items-center justify-center mt-48 md:mt-96">
-				<div className="w-full md:w-1/2">
-					{/* <ImageSVG /> */}
-					<img
-						className="w-2/3 mx-auto md:ml-60"
-						src={aboutMe}
-						alt="about me"
-					/>
-				</div>
-				<div className=" w-full md:w-1/2 px-4 md:pr-64 py-8 md:py-16">
+			<div className="flex flex-wrap items-center justify-center mt-48 md:mt-96">
+				<div className="w-full lg:w-1/2 lg:px-32 px-5">
 					<h1 className="text-center text-3xl md:text-5xl font-bold text-gray-800 mb-14">
 						Acerca de mi
-					</h1>
-					<p className="text-gray-600 mb-4 md:text-left text-center">
+					</h1>	
+					<p className="text-gray-600 mb-4 text-center">
 						Soy un desarrollador web con experiencia en desarrollo
 						de aplicaciones web, con conocimientos y habilidades en
 						tecnologías de backend y frontend. Como estudiante de
@@ -31,6 +23,14 @@ export const AboutMe = () => {
 						conocimiento y ayudar a otros a desarrollar sus
 						habilidades en el área.
 					</p>
+				</div>
+				<div className="w-full lg:w-1/2 justify-center lg:mt-0 mt-20">
+					{/* <ImageSVG /> */}
+					<img
+						className="w-2/3 mx-auto lg:mx-0"
+						src={aboutMe}
+						alt="about me"
+					/>
 				</div>
 			</div>
 		</div>
